09e1686821d2ad6b31d38952e3cbb24304bb752f
Andreas Eckner <andreas.eckner@gmail.com>
2023-04-11 17:37:01 +0200

    Add exercise for 3.02 - RecyclerView - Optimisation

--- app/src/main/java/com/example/othregensburg/zapp/ItemAdapter.kt
+++ app/src/main/java/com/example/othregensburg/zapp/ItemAdapter.kt
@@ -9,8 +9,16 @@ import androidx.recyclerview.widget.RecyclerView
 class ItemAdapter(private val data: List<String>) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+    // TODO (1) Let the ViewHolder implement a View.OnClickListener
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val itemCheckBox: CheckBox = view.findViewById(R.id.checkbox)
+
+        // TODO (2) Set the OnCLickListener to the checkboxes
+
+        // TODO (3) Show a Toast with position and state of the clicked checkbox
+
+        // TODO (4) Fix the issue with checkbox state of the recycled views which occurs after scrolling up and down
+        // Hint: Make use of a SpareBooleanArray
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
